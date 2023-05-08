5c91da1deecab323f795b31f0ed770ac69eb594f
Brad <brad.e.stover@protonmail.com>
2023-05-07 19:13:58 -0500

    final

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq; 
+namespace DA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DATA ANALYSIS");
+            JsonReadService.ReadJsonFile(AskForJsonFileName());
+            
+        }
+
+        public static string AskForJsonFileName()
+        {
+        BEGIN:
+            Console.Write("\nName of JSON file? ");
+            var response = Console.ReadLine();
+           if (File.Exists(response))
+            {
+                Console.Write(Environment.NewLine);
+                return response;
+            }
+           else
+            {
+               Console.Write("\nError: File doesn't exist!");
+               goto BEGIN;
+            }
+        }
+    }
+}
